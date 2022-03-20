69d8afaa514dd14b3d50bdde772035f7dfd2b0b7
Devesh Pal <newdev0@outlook.com>
2022-03-20 22:40:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ fastify.get('/', async (req, reply) => {
 
 fastify.get('/generate', async (request, reply) => {
   var darkMode = request.query.darkMode ? JSON.parse(request.query.darkMode) : false
-  var title = request.query.title ? request.query.title : "RaySo"
-  var theme = request.query.theme ? request.query.theme : "raindrop"
-  var lang = request.query.lang ? request.query.lang : "auto"
+  var title = request.query.title || "RaySo"
+  var theme = request.query.theme || "raindrop"
+  var lang = request.query.lang || "auto"
   var bg = request.query.bg ? JSON.parse(request.query.bg) : true
   var padding = request.query.padding ? JSON.parse(request.query.padding) : 64
-  var text = request.query.text ? request.query.text : ""
+  var text = request.query.text || ""
   if (!text) {
     return { "error": true, "message": "Provide text"}
   }
   if (![16, 32, 64, 128].includes(padding)) {
     return { "error": true, "message": "padding must be one of 16, 32, 64, 128" }
   }
-  if (!["breeze", "candy", "crimson", "falcon", "meadow", "raindrop", "sunset", "midnight"]) {
+  if (!["breeze", "candy", "crimson", "falcon", "meadow", "raindrop", "sunset", "midnight"].includes(theme)) {
     return { "error": true, "message": "Available themes: breeze, candy, crimson, falcon, meadow, midnight, raindrop, sunset!" }
   }
-  console.log(process.env.BROWSER_PATH)
+//  console.log(process.env.BROWSER_PATH)
   var raySo = new RaySo({
     "title": title,
     "darkMode": darkMode,
@@ -52,10 +52,10 @@ fastify.get('/generate', async (request, reply) => {
 
 fastify.post('/generate', async (request, reply) => {
   var darkMode = request.query.darkMode ? JSON.parse(request.query.darkMode) : false
-  var title = request.query.title || "RaySo"
-  var theme = request.query.theme || "raindrop"
-  var lang = request.query.lang || "auto"
-  var bg = request.query.bg ? JSON.parse(request.query.bg) : true
+  var title = request.body.title || "RaySo"
+  var theme = request.body.theme || "raindrop"
+  var lang = request.body.lang || "auto"
+  var bg = request.body.bg ? JSON.parse(request.query.bg) : true
   var padding = request.query.padding ? JSON.parse(request.query.padding) : 64
   if (![16, 32, 64, 128].includes(padding)) {
     return { "error": true, "message": "padding must be one of 16, 32, 64, 128" }
