2f4e52fe59856cad822044ce74fe7313d7d1603d
TechiError <techierror@gmail.com>
2022-03-20 21:34:47 +0530

    Add GET request

--- index.js
+++ index.js
@@ -11,8 +11,46 @@ fastify.get('/', async (req, reply) => {
   return reply.sendFile('index.html')
 })
 
+fastify.get('/generate', async (request, reply) => {
+  var darkMode = request.query.darkMode ? JSON.parse(request.query.darkMode) : false
+  var title = request.query.title ? request.query.title : "RaySo"
+  var theme = request.query.theme ? request.query.theme : "raindrop"
+  var lang = request.query.lang ? request.query.lang : "auto"
+  var bg = request.query.bg ? JSON.parse(request.query.bg) : true
+  var padding = request.query.padding ? JSON.parse(request.query.padding) : 64
+  var text = request.query.text ? request.query.text : ""
+  if (!text) {
+    return { "error": true, "message": "Provide text"}
+  }
+  if (![16, 32, 64, 128].includes(padding)) {
+    return { "error": true, "message": "padding must be one of 16, 32, 64, 128" }
+  }
+  if (!["breeze", "candy", "crimson", "falcon", "meadow", "raindrop", "sunset", "midnight"]) {
+    return { "error": true, "message": "Available themes: breeze, candy, crimson, falcon, meadow, midnight, raindrop, sunset!" }
+  }
+  console.log(process.env.BROWSER_PATH)
+  var raySo = new RaySo({
+    "title": title,
+    "darkMode": darkMode,
+    "theme": theme,
+    "language": lang,
+    "background": bg,
+    "padding": padding,
+    "browserPath": process.env.BROWSER_PATH
+  });
+
+  raySo
+    .cook(text)
+    .then((response) => {
+      reply.type('image/png');
+      reply.send(response)
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+})
+
 fastify.post('/generate', async (request, reply) => {
-  console.log(request.body)
   var darkMode = request.query.darkMode ? JSON.parse(request.query.darkMode) : false
   var title = request.query.title ? request.query.title : "RaySo"
   var theme = request.query.theme ? request.query.theme : "raindrop"
